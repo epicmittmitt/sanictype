86f59c48f75740548d01c3f7f79cb9e3757f9bcc
Sumit Basak <mittsquared@protonmail.com>
2017-10-14 20:51:45 -0400

    Bug Fixes
    - Apparently nim is not as versatile as I thought
    - Reverted some refactorings

--- sanic.nim
+++ sanic.nim
@@ -12,32 +12,45 @@ for str in readFile("db.txt").split:
 proc complete(frag:string, i: int): string = 
   var counter = 0
   for str in db:
-    if str.startsWith(frag) and counter == i:
-      return str.replace(frag, "")
-    counter += 1
+    if str.startsWith(frag):
+      if counter == i:
+        return str.replace(frag, "")
+      counter += 1
 proc writeString(input: string) = 
   for c in input:
-    keybd_event(VkKeyScan(c.BYTE).BYTE, 0x9e.BYTE, 0, 0);
-    keybd_event(VkKeyScan(c.BYTE).BYTE, 0x9e.BYTE, KEYEVENTF_KEYUP, 0);
+    keybd_event(VkKeyScan(c.BYTE).BYTE, 0x9e.BYTE, 0, 0)
+    keybd_event(VkKeyScan(c.BYTE).BYTE, 0x9e.BYTE, KEYEVENTF_KEYUP, 0)
 proc backspace(num: int) =
   for i in 0..<num:
     keybd_event(VK_BACK, 0x9e.BYTE, 0, 0);
     keybd_event(VK_BACK, 0x9e.BYTE, KEYEVENTF_KEYUP, 0);
+proc reset() =
+  frag = ""
+  tabs = 0
+  lastString = ""
 proc main() =
   while true:
     Sleep(50)
     for i in 8..190:
       if GetAsyncKeyState(cint(i)) == -32767:
-        proc reset() =
-          frag = ""
-          tabs = 0
-          lastString = ""
         if VK_SPACE == i or VK_RETURN == i: reset()
         elif VK_BACK == i: frag.delete(frag.len, frag.len)
         elif chr(i) in Letters: frag = frag & toLowerAscii($chr(i))
-        if frag == "sanicon": on = true
-        if frag == "sanicoff": on = false
+        var delFrag = false
+        if frag == "sanicon":
+          on = true
+          delFrag = true
+        if frag == "sanicoff":
+          on = false
+          delFrag = true
+        if frag == "sanicexit":
+          backspace("sanicexit".len)
+          return
+        if delFrag:
+          backspace(frag.len)
+          reset()
         if VK_TAB == i and on:
+          echo lastString, frag
           backspace(lastString.len + 1)
           lastString = complete(frag.strip, tabs)
           writeString(lastString)
